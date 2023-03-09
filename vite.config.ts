baa0478c3849c163c51c0484efd6658446a344ee
Daniel8bits <daniel.sftm@outlook.com>
2023-03-08 22:52:16 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
-			name: 'onyx-ui',
-			fileName: 'onyx-ui',
+			name: 'aquino',
+			fileName: 'aquino',
 		},
 	},
 	test: {
