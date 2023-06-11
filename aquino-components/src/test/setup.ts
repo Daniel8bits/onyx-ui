fe4cd69f628f2a30153bc4bcf5f428a714cb6eae
daniel <daniel.sftm@outlook.com>
2023-06-11 04:14:08 -0300

    cleaning test files and vitest configs

--- aquino-components/src/test/setup.ts
+++
@@ -1,7 +0,0 @@
-import matchers from '@testing-library/jest-dom';
-import {expect} from 'vitest';
-import ResizeObserver from 'resize-observer-polyfill';
-
-expect.extend(matchers);
-
-global.ResizeObserver = ResizeObserver;
